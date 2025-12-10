7f2eca7c0a0351003d42ec9543fa06156dc1ac26
tianquege <wangn5635@gmail.com>
2025-12-10 21:51:25 +0800

    fix(android): Update NDK version and enable core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,11 +18,12 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.v2board.v2board_admin"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -57,6 +58,10 @@ android {
             }
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
